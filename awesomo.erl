082534d3c8d2ff5c6302a7af28dbaad59e091fe0
Líkið Geimfari <likid.geimfari@gmail.com>
2017-12-29 23:08:24 +0300

    Update and rename awesomo.erl to awesomo.rs

--- awesomo.erl
+++
@@ -1,4 +0,0 @@
--module(hello).
--export([hello_world/0]).
-
-hello_world() -> io:fwrite("hello, world\n").
---
+++ awesomo.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
